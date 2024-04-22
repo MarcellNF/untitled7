ad032d06f6c33603765d1bab351e1c7b58d6a439
marce <marcell.dechant@proton.me>
2024-04-22 10:11:37 +0200

    BIG BANG

---
+++ src/test/java/HelloWorldTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class HelloWorldTest {
+    @Test
+    void helloWorld() {
+        assertEquals("Hello World", "Hello World");
+    }
+}
